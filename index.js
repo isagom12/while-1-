2d1479f378cafd98e673e103dd5278bfec33959b
isa gomes <161726701+isagom12@users.noreply.github.com>
2024-08-28 10:47:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+  const prompt = require('prompt-sync');
+  const entrada = prompt();
+  var numero = entrada('Digite um número para somar');
+  var soma = 0;
+  while(numero != 0){
+    console.log(`Bom dia`);
+    numero = entrada(`Digite um novo valor:`);
+  }
